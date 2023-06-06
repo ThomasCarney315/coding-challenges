9bb3aafe95922bcd3295ca2f1bbc187a03248a05
ThomasCarney315 <thomascarney315@gmail.com>
2023-06-06 17:36:39 +0300

    string challenge 1 of 5

---
+++ javascript/codewars/7kyu/eliminate-the-intruders-bit-manipulation.js
@@ -0,0 +1,17 @@
+// Task
+// You are given a string representing a number in binary. Your task is to delete all the unset bits in this string and return the corresponding number (after keeping only the '1's).
+
+// In practice, you should implement this function:
+
+// function eliminateUnsetBits(number);
+// Examples
+// eliminateUnsetBits("11010101010101") ->  255 (= 11111111)
+// eliminateUnsetBits("111") ->  7
+// eliminateUnsetBits("1000000") -> 1
+// eliminateUnsetBits("000") -> 0
+// BITS STRINGS FUNDAMENTALS
+
+function eliminateUnsetBits(number) {
+    let newNum = parseInt(number.replaceAll('0',''),2)
+    return newNum ? newNum : 0
+  }
