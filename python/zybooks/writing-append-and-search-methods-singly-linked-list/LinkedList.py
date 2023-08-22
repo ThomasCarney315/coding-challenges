31bf9b5e976d521aa61cb9bb2a2e2811ff5e5ec2
ThomasCarney315 <thomascarney315@gmail.com>
2023-08-22 17:11:11 -0400

    python challenge pre-solution commit

---
+++ python/zybooks/writing-append-and-search-methods-singly-linked-list/LinkedList.py
@@ -0,0 +1,47 @@
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # TODO: Write append() method
+
+    def prepend(self, new_node):
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+    def insert_after(self, current_node, new_node):
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        elif current_node is self.tail:
+            self.tail.next = new_node
+            self.tail = new_node
+        else:
+            new_node.next = current_node.next
+            current_node.next = new_node
+            
+    def remove_after(self, current_node):
+        # Special case, remove head
+        if (current_node == None) and (self.head != None):
+            succeeding_node = self.head.next
+            self.head = succeeding_node  
+            if succeeding_node == None: # Remove last item
+                self.tail = None
+        elif current_node.next != None:
+            succeeding_node = current_node.next.next
+            current_node.next = succeeding_node
+            if succeeding_node == None: # Remove tail
+                self.tail = current_node
+
+    # TODO: Write search() method that locates node with the same data value 
+    #       (data) as the key and sets the found node's position (node_pos)
+        
+    def print_list(self):
+        cur_node = self.head
+        while cur_node != None:
+            cur_node.print_node_data()
+            cur_node = cur_node.next
