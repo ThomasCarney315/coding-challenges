6bd19324f63f4cd58e36aa3b6abbe2dae84513a1
ThomasCarney315 <thomascarney315@gmail.com>
2023-09-04 14:39:49 -0400

    python challenge complete

--- python/zybooks/implement-a-stack-using-an-array/Stack.py
+++ python/zybooks/implement-a-stack-using-an-array/Stack.py
@@ -4,8 +4,31 @@ class Stack:
         self.top_index = -1
 
     # TODO: Write a method to push a value into the stack
+    def push(self, el):
+        i = self.top_index
+        if i == 4:
+            self.print_full()
+            return
+        else:
+            self.array[i + 1] = el
+            self.top_index += 1
 
     # TODO: Write a method to pop the top value from the stack
+    def pop(self):
+        i = self.top_index
+        if i == -1:
+            self.print_empty()
+        else:
+            self.array[i] = None
+            self.top_index -= 1
+            
+    # Print method for full stack
+    def print_full(self):
+        print("Can't push, stack is full" )
+
+    # Print method for empty stack    
+    def print_empty(self):
+        print("Can't pop, stack is empty")
 
     def print_stack(self):
         i = self.top_index
