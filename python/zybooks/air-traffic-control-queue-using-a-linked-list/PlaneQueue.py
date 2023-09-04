c601596ad30c7167b2391e401b418f08b99a0899
ThomasCarney315 <thomascarney315@gmail.com>
2023-09-04 12:03:19 -0400

    python challenge pre-solution commit

---
+++ python/zybooks/air-traffic-control-queue-using-a-linked-list/PlaneQueue.py
@@ -0,0 +1,34 @@
+from PlaneList import PlaneList
+
+class PlaneQueue:
+    def __init__(self):
+        self.plane_list = PlaneList()
+        self.length = 0
+    
+    # TODO: Write push() and pop() methods. push() adds an item to the queue
+    #       and adds 1 to length. pop() removes and returns the first item in 
+    #       the queue and subtracts 1 from length.
+
+
+    def is_empty(self):
+        return self.length == 0
+        
+    def print_queue(self):
+        print('Air-traffic control queue')
+        if not self.is_empty():
+            print('   Next to land:', end=' ')
+            cur_node = self.plane_list.head
+            cur_node.print_node_data()
+            print()
+            
+            if self.length > 1:
+                print('   Arriving flights:')
+                cur_node = cur_node.next
+                while cur_node is not None:
+                    print('      ', end='')
+                    cur_node.print_node_data()
+                    print()
+                    cur_node = cur_node.next
+        else:
+            print('Queue is empty.')
+        print()
---
+++ python/zybooks/air-traffic-control-queue-using-a-linked-list/main.py
@@ -0,0 +1,11 @@
+from PlaneQueue import PlaneQueue
+from PlaneNode import PlaneNode
+
+if __name__ == "__main__":
+    plane_queue = PlaneQueue()
+    
+    # TODO: Read in arriving flight codes and whether a flight has landed. 
+    #       Print the queue after every push() or pop() operation. If the user
+    #       entered "landed", print which flight has landed. Continue until -1 
+    #       is read.
+    
